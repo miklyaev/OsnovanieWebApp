56e8fd73c70ca5702965504e3ed82547e592558e
Miklyaev Sergey <miklyaev_sv@astralnalog.ru>
2023-02-27 23:44:41 +0700

    Добавил SignalR

---
+++ SignalRApp/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRApp
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await this.Clients.All.SendAsync("Receive", message, userName);
+        }
+    }
+}
---
+++ SignalRApp/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Http.Connections;
+using SignalRApp;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// подключем сервисы SignalR
+//Глобальная настройка хабов:
+builder.Services.AddSignalR(hubOptions =>
+{
+    hubOptions.EnableDetailedErrors = true;
+    hubOptions.KeepAliveInterval = TimeSpan.FromMinutes(1);
+});
+
+//Настройка только для хаба ChatHub:
+//builder.Services.AddSignalR().AddHubOptions<ChatHub>(options =>
+//{
+//    options.EnableDetailedErrors = true;
+//    options.KeepAliveInterval = System.TimeSpan.FromMinutes(1);
+//});
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+//app.MapHub<ChatHub>("/chat");   // ChatHub будет обрабатывать запросы по пути /chat
+//вариант с настройками
+app.MapHub<ChatHub>("/chat",
+    options => {
+        options.ApplicationMaxBufferSize = 128;
+        options.TransportMaxBufferSize = 128;
+        options.LongPolling.PollTimeout = TimeSpan.FromMinutes(1);
+        options.Transports = HttpTransportType.LongPolling | HttpTransportType.WebSockets;
+    });
+
+app.Run();
